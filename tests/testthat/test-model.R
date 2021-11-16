b69a88349db6ae0a1a6d4b7d5f160b84362ac963
Lilith Whittles <lilithwhittles@gmail.com>
2021-11-16 15:22:51 +0000

    update model tests

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -1,35 +1,25 @@
-
-pars <- list(U0 = 5e7,
-             A0 = 6e6,
-             beta = 0.01,
-             sigma = 0.1,
-             t_s = 100,
-             p_S = 0.6,
-             p_R = 0.3,
-             p_I = 0.0001,
-             p_F = 0.001,
-             delta_A = 30,
-             delta_E = 2,
-             delta_I = 14,
-             delta_S = 2.3,
-             delta_F = 7,
-             delta_R = 365 * 5,
-             alpha = 7e5 / 365,
-             omega = 0.01)
+pars <- transform(example_gas_parameters())
 
 test_that("model runs", {
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$simulate(7)
   idx <- unlist(mod$info()$index)
 
   # check that states sum to N
   expect_equal(colSums(y[seq(2, 9), , ]), y[idx["N"], , ])
-  expect_true(all(y > 0))
+  expect_true(all(y >= 0))
+
+  tmp <- c(1, 25700346, 5359260, 12089, 0, 2345, 229, 0, 24929013, 56003282,
+           1, 25700753, 5359326, 11754, 0, 2401, 252, 0, 24927187, 56001673,
+           1, 25699514, 5358338, 11795, 1, 2354, 247, 0, 24928223, 56000472,
+           1, 25700244, 5359021, 11898, 0, 2489, 257, 0, 24926944, 56000853,
+           1, 25700111, 5358832, 11903, 2, 2444, 245, 0, 24927715, 56001252)
+  expect_equal(y, array(tmp, dim = c(10L, 5L, 1L)))
 })
 
 test_that("there are no infections when beta is 0", {
   pars$beta <- 0
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$E == 0))
@@ -44,7 +34,7 @@ test_that("there are no infections when beta is 0", {
 
 test_that("there are no infections when A0 = 0", {
   pars$A0 <- 0
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$E == 0))
@@ -53,7 +43,7 @@ test_that("there are no infections when A0 = 0", {
   expect_true(all(y$S2 == 0))
   expect_true(all(y$I == 0))
   expect_true(all(y$F == 0))
-  expect_true(all(y$R == 0))
+  expect_true(all(y$R > 0))
 
   expect_true(all(unlist(y) >= 0))
 })
@@ -61,7 +51,7 @@ test_that("there are no infections when A0 = 0", {
 
 test_that("there are no symptomatic infections when p_S = 0", {
   pars$p_S <- 0
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$E == 0))
@@ -78,7 +68,7 @@ test_that("there are no asymptomatic infections when p_S = 1", {
   pars$p_S <- 1
   pars$E0 <- pars$A0
   pars$A0 <- 0
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$A == 0))
@@ -93,7 +83,7 @@ test_that("there are no asymptomatic infections when p_S = 1", {
 
 test_that("there is no iGAS when p_I = 0", {
   pars$p_I <- 0
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$A > 0))
@@ -101,7 +91,7 @@ test_that("there is no iGAS when p_I = 0", {
   expect_true(all(y$S1 > 0))
   expect_true(all(y$S2 > 0))
   expect_true(all(y$I == 0))
-  expect_true(all(y$F > 0))
+  expect_true(any(y$F > 0))
   expect_true(all(y$R > 0))
 
   expect_true(all(unlist(y) >= 0))
@@ -109,7 +99,7 @@ test_that("there is no iGAS when p_I = 0", {
 
 test_that("there is no pharyngitis when p_I = 1", {
   pars$p_I <- 1
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$A > 0))
@@ -125,14 +115,14 @@ test_that("there is no pharyngitis when p_I = 1", {
 
 test_that("there is no scarlet fever when p_F = 0", {
   pars$p_F <- 0
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$A > 0))
   expect_true(all(y$E > 0))
   expect_true(all(y$S1 > 0))
   expect_true(all(y$S2 > 0))
-  expect_true(all(y$I > 0))
+  expect_true(any(y$I > 0))
   expect_true(all(y$F == 0))
   expect_true(all(y$R > 0))
 
@@ -141,14 +131,14 @@ test_that("there is no scarlet fever when p_F = 0", {
 
 test_that("there is no pharyngitis when p_F = 1", {
   pars$p_F <- 1
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$A > 0))
   expect_true(all(y$E > 0))
   expect_true(all(y$S1 > 0))
   expect_true(all(y$S2 == 0))
-  expect_true(all(y$I > 0))
+  expect_true(any(y$I > 0))
   expect_true(all(y$F > 0))
   expect_true(all(y$R > 0))
 
@@ -158,7 +148,7 @@ test_that("there is no pharyngitis when p_F = 1", {
 test_that("there is no immunity when p_S = 0 and p_R = 0", {
   pars$p_S <- 0
   pars$p_R <- 0
-  mod <- model$new(pars, 0, 5)
+  mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
 
   expect_true(all(y$A > 0))
@@ -167,7 +157,7 @@ test_that("there is no immunity when p_S = 0 and p_R = 0", {
   expect_true(all(y$S2 == 0))
   expect_true(all(y$I == 0))
   expect_true(all(y$F == 0))
-  expect_true(all(y$R == 0))
+  expect_true(all(y$R > 0))
 
   expect_true(all(unlist(y) >= 0))
 })
