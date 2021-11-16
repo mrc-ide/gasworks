c06ea90421e88b8849eae18090a60765cb3af601
Lilith Whittles <lilithwhittles@gmail.com>
2021-11-16 09:44:38 +0000

    add date functions

---
+++ R/support.R
@@ -0,0 +1,57 @@
+
+##' @name model_day
+##' @title Convert date to model_day
+##' @description Convert date to model_day
+##' @param date A vector of dates or character strings containing dates
+##' @return A vector of model_days (i.e. integers),
+##' where model_day 1 = 1 Jan 2014
+##' @examples
+##' model_day("2014-01-01")
+##' model_day("2014-01-07")
+##' @export
+model_day <- function(date) {
+  start_date <- as.Date("2013-12-31")
+  as.numeric(as.Date(date) - start_date)
+}
+
+##' @name model_date
+##' @title Convert model_day to date
+##' @description Convert model_day to date
+##' @param date A vector of model_days (i.e. integers),
+##'  where model_day 1 = 1 Jan 2014
+##' @return A vector of dates
+##' @examples
+##' model_date(1)
+##' model_date(7)
+##' @export
+model_date <- function(day) {
+  as.Date("2013-12-31") + day
+}
+
+##' @name model_week
+##' @title Convert date to model_week
+##' @description Convert date to model_week
+##' @param date A vector of dates or character strings containing dates
+##' @return A vector of model_weeks (i.e. integers),
+##'  where model_week 1 = the week ending 7 Jan 2014
+##' @examples
+##' model_week("2014-01-01")
+##' model_week("2014-01-07")
+##' @export
+model_week <- function(date) {
+  ceiling(model_day(date) / 7)
+}
+
+##' @name model_week_date
+##' @title Convert model_week to date on which the week ends
+##' @description Convert model_week to date on which the week ends
+##' @param date A vector of model_weeks (i.e. integers),
+##'  where model_week 1 ends on 7 Jan 2014
+##' @return A vector of dates giving the date on which the week ends
+##' @examples
+##' model_week_date(1)
+##' model_week_date(7)
+##' @export
+model_week_date <- function(week) {
+  model_date(week * 7)
+}
