eea4370d752c895c2543a1d3c66778a5fe77c8ad
Lilith Whittles <lilithwhittles@gmail.com>
2021-11-23 10:15:25 +0000

    import abind

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -1,7 +1,7 @@
 
 
 test_that("model runs", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$simulate(7)
   rownames(y) <- names(mod$info()$index)
@@ -11,7 +11,7 @@ test_that("model runs", {
   expect_true(all(y >= 0))
 
 
-  tmp <- c(1, 21687448, 6174233, 1112981, 159, 950850, 569569,
+  y <- c(1, 21687448, 6174233, 1112981, 159, 950850, 569569,
            749, 25514822, 56010811, 4950895, 897364, 892, 179, 700590, 689779,
            0.254421609239827, 1, 21685182, 6176629, 1112823, 169, 949518,
            569192, 776, 25515215, 56009504, 4951768, 896877, 910, 182, 699526,
@@ -22,11 +22,11 @@ test_that("model runs", {
            897336, 857, 208, 699963, 689591, 0.254319583239358, 1, 21680889,
            6178399, 1113810, 160, 951629, 570990, 747, 25513261, 56009885,
            4956400, 897754, 892, 180, 700225, 690340, 0.254589465871049)
-  expect_equivalent(y, array(tmp, dim = c(17L, 5L, 1L)))
+  expect_equivalent(y, array(y, dim = c(17L, 5L, 1L)))
 })
 
 test_that("there are no infections when beta is 0", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$beta <- 0
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
@@ -43,7 +43,7 @@ test_that("there are no infections when beta is 0", {
 })
 
 test_that("there are no infections when A0 = 0", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$A0 <- 0
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
@@ -62,7 +62,7 @@ test_that("there are no infections when A0 = 0", {
 
 
 test_that("there are no symptomatic infections when p_S = 0", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$p_S <- 0
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
@@ -79,6 +79,7 @@ test_that("there are no symptomatic infections when p_S = 0", {
 })
 
 test_that("there are no asymptomatic infections when p_S = 1", {
+  pars <- example_gas_parameters()
   pars$p_S <- 1
   pars$E0 <- pars$A0
   pars$A0 <- 0
@@ -97,7 +98,7 @@ test_that("there are no asymptomatic infections when p_S = 1", {
 })
 
 test_that("there is no iGAS when p_I = 0", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$p_I <- 0
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
@@ -115,7 +116,7 @@ test_that("there is no iGAS when p_I = 0", {
 })
 
 test_that("there is no pharyngitis when p_I = 1", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$p_I <- 1
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
@@ -133,7 +134,7 @@ test_that("there is no pharyngitis when p_I = 1", {
 })
 
 test_that("there is no scarlet fever when p_F = 0", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$p_F <- 0
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
@@ -151,7 +152,7 @@ test_that("there is no scarlet fever when p_F = 0", {
 })
 
 test_that("there is no pharyngitis when p_F = 1", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$p_F <- 1
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- mod$transform_variables(mod$simulate(7))
@@ -169,7 +170,7 @@ test_that("there is no pharyngitis when p_F = 1", {
 })
 
 test_that("there is no immunity when p_S = 0 and p_R = 0", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   pars$p_S <- 0
   pars$p_R <- 0
   mod <- model$new(pars, 0, 5, seed = 1L)
@@ -189,45 +190,73 @@ test_that("there is no immunity when p_S = 0 and p_R = 0", {
 
 
 test_that("the foi is calculated correctly", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- lapply(seq(0, 365 * 5, 7), mod$simulate)
-  tmp <- abind::abind(y, along = 3L)
-  rownames(tmp) <- names(model_index())
-  t <- tmp["time", 1, ]
-  par(mar = c(3, 3, 1, 1), mgp = c(1.5, 0.5, 0))
-  cols <- 2:4
-  N <- tmp["N", , ]
-  infectious <- tmp["A", , ] + tmp["S1", , ] + tmp["S2", , ]
-  I <- tmp["E", , ] + infectious + tmp["F", , ] + tmp["I", , ]
-  par(mfrow = c(1, 2))
-  matplot(t, t(tmp["U", , ] / N), type = "l", ylim = c(0, 1),
-          col = cols[1])
-  matlines(t, t(I / N), type = "l", col = cols[2])
-  matlines(t, t(tmp["R", , ] / N), type = "l", col = cols[3])
-  legend("topright", legend = c("S", "I", "R"), fill = cols)
+  y <- abind::abind(y, along = 3L)
+  rownames(y) <- names(model_index())
+  t <- y["time", 1, ]
+  par(mfrow = c(2, 2), mar = c(3, 3, 1, 1), mgp = c(1.5, 0.5, 0), bty = "n")
+  N <- y["N", , ]
+  infectious <- y["A", , ] + y["S1", , ] + y["S2", , ]
+  I <- y["E", , ] + infectious + y["F", , ] + y["I", , ]
+  matplot(t, t(y["U", , ] / N), type = "l", ylim = c(0, 1),
+          col = 2, ylab = "%")
+  matlines(t, t(I / N), type = "l", col = 3)
+  matlines(t, t(y["R", , ] / N), type = "l", col = 4)
+  legend("topright", legend = c("S", "I", "R"), fill = 2:4, bty = "n")
 
-  foi <- t(tmp["foi", , ] / infectious * N)
+  foi <- t(y["foi", , ] / infectious * N)
   max_date <- model_week_date(t[apply(foi, 2, which.max)])
+  # when there is a seasonal effect all max foi dates should be the same and
+  # in the same week as the input t_s
   expect_true(all(max_date == max_date[1]))
   expect_true(as.numeric(max_date[1] - model_date(pars$t_s)) %% 365 <= 7)
 
   matplot(t, foi, type = "l", col = 1)
-  matplot(t, t(tmp["igas_inc", , ]), type = "l", col = 1)
-  matplot(t, t(tmp["scarlet_fever_inc", , ]), type = "l", col = 1)
+  matplot(t, t(y["igas_inc", , ]), type = "l", col = 1, ylab = "iGAS / 100k")
+  matplot(t, t(y["scarlet_fever_inc", , ]), type = "l", col = 1,
+          ylab = "SF / 100k")
 
   pars$sigma <- 0
   mod <- model$new(pars, 0, 5, seed = 1L)
   y <- lapply(seq(0, 365 * 5, 7), mod$simulate)
-  tmp <- abind::abind(y, along = 3L)
-  rownames(tmp) <- names(model_index())
-  infectious <- tmp["A", , ] + tmp["S1", , ] + tmp["S2", , ]
-  foi <- t(tmp["foi", , ] / infectious * tmp["N", , ])
-  matplot(t, foi, type = "l")
+  y <- abind::abind(y, along = 3L)
+  rownames(y) <- names(model_index())
+  infectious <- y["A", , ] + y["S1", , ] + y["S2", , ]
+  foi <- t(y["foi", , ] / infectious * y["N", , ])
+  max_date <- model_week_date(t[apply(foi, 2, which.max)])
+
+  # when there is no seasonal effect, date of max foi should differ by particle
+  expect_true(any(max_date != max_date[1]))
+  expect_true(as.numeric(max_date[1] - model_date(pars$t_s)) %% 365 > 7)
 })
 
 
 test_that("incidence time series output correctly", {
-  pars <- transform(example_gas_parameters())
+  pars <- example_gas_parameters()
+  pars$omega <- 0
+  pars$alpha <- 0
+  pars$delta_I <- 1e6
+  pars$delta_F <- 1e6
+  mod <- model$new(pars, 0, 5, seed = 1L)
+  y <- lapply(seq(0, by = 7, length.out = 5), mod$simulate)
+  y <- abind::abind(y, along = 3L)
+  rownames(y) <- names(model_index())
+  expect_equal(y["F", , 5], rowSums(y["scarlet_fever_inc", , ]))
+  expect_equal(y["F", , 2], y["scarlet_fever_inc", , 2])
+  expect_equal(y["I", , 5], rowSums(y["igas_inc", , ]))
+  expect_equal(y["I", , 2], y["igas_inc", , 2])
+  expect_true(all(y["entrants_inc", ,] == 0))
+  expect_true(all(y["leavers_inc", ,] == 0))
 
+  pars$delta_S <- 1e6
+  mod <- model$new(pars, 0, 5, seed = 1L)
+  y <- lapply(seq(0, by = 7, length.out = 5), mod$simulate)
+  y <- abind::abind(y, along = 3L)
+  rownames(y) <- names(model_index())
+  expect_equal(y["S2", , 5],
+               rowSums(y["pharyngitis_inc", , ] - y["scarlet_fever_inc", , ]))
+  expect_equal(y["S2", , 2],
+               y["pharyngitis_inc", , 2] - y["scarlet_fever_inc", , 2])
 })
