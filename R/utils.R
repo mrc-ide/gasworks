f4f459247f317a0b3ef0fb2269cdc6937545753d
Lilith Whittles <lilithwhittles@gmail.com>
2021-11-25 11:44:59 +0000

    update assertions to include optional length check

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,10 @@
 `%||%` <- function(a, b) { # nolint
   if (is.null(a)) b else a
 }
+
+named_list <- function(names, x = NULL) {
+  ret <- vector("list", length(names))
+  names(ret) <- names
+  if (!is.null(x)) ret[] <- x
+  ret
+}
