a3da410112413e46d9daa9694daf01ada8c2791d
Lilith Whittles <lilithwhittles@gmail.com>
2022-01-07 09:35:08 +0000

    add helper function for age groups

---
+++ R/helium.R
@@ -0,0 +1,12 @@
+
+##' @name helium_age_groups
+##' @title Helium model age groups
+##' @description Helium model age groups
+##' @return A list of named model parameters
+##' @export
+helium_age_groups <- function() {
+  age <- c(seq(0, 90, 5), 100)
+  list(age_start = age[-length(age)],
+       age_end = age[-1],
+       n_group = length(age))
+}
