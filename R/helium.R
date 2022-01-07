3d60ef76340a88ef7152addba15b4a4493e582ba
Lilith Whittles <lilithwhittles@gmail.com>
2022-01-07 15:46:27 +0000

    quick test of helium groups

--- R/helium.R
+++ R/helium.R
@@ -8,5 +8,5 @@ helium_age_groups <- function() {
   age <- c(seq(0, 90, 5), 100)
   list(age_start = age[-length(age)],
        age_end = age[-1],
-       n_group = length(age))
+       n_group = length(age) - 1)
 }
