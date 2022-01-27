7c5cd67043d6bd24c873ba5fe1c5ec7810694407
Lilith Whittles <lilithwhittles@gmail.com>
2022-01-27 13:11:22 +0000

    test utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,8 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% NULL, NULL)
   expect_equal(NULL %||% 2, 2)
 })
+
+test_that("named_list", {
+  expect_equal(named_list(letters[1:3], 1:3), list(a = 1, b = 2, c = 3))
+  expect_equal(named_list(letters[1:3]), list(a = NULL, b = NULL, c = NULL))
+})
