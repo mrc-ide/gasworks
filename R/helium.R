12f53ca9fc2a5e400a6c9636afd170815fab0ef0
Lilith Whittles <lwhittles@users.noreply.github.com>
2022-02-28 19:34:57 +0000

    update tests

--- R/helium.R
+++ R/helium.R
@@ -5,7 +5,7 @@
 ##' @return A list of named model parameters
 ##' @export
 helium_age_groups <- function() {
-  age <- c(seq(0, 90, 5), 100)
+  age <- c(seq(0, 75, 5), 100)
   list(age_start = age[-length(age)],
        age_end = age[-1],
        n_group = length(age) - 1)
