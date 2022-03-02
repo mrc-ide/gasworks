aeab5efde83615f8cf70037c362397816df8e661
Lilith Whittles <lwhittles@users.noreply.github.com>
2022-03-02 18:02:16 +0000

    move hydrogen_compare into package

--- R/hydrogen.R
+++ R/hydrogen.R
@@ -29,3 +29,42 @@ hydrogen_index <- function(info) {
   list(run = unlist(info$index[run]),
        state = unlist(info$index[c(run, save)]))
 }
+
+##' Compare observed and modelled data from the hydrogen model. This
+##' conforms to the mcstate interface.
+##' @title Compare observed and modelled data for the hydrogen model
+##' @param state State vector for the end of the current week. This is
+##' assumed to be filtered following [hydrogen_index()] so contains
+##' rows corresponding to average daily pharyngitis rate per 100,000,
+##' weekly number of scarlet fever cases and weekly number of iGAS cases.
+##' @param observed Observed data. This will be a list with elements
+##' `daily_pharyngitis_rate`, `scarlet_fever_inc` and `igas_inc`
+##' @param pars A list of parameters, as created by [model_parameters()]
+##' @return A vector of log likelihoods, the same length as the number
+##'   of particles (the number of columns in the modelled state)
+##' @export
+##' @examples
+##' state <- rbind(daily_pharyngitis_rate = 10:15,
+##'                scarlet_fever_inc = 100:105,
+##'                igas_inc = 90:95)
+##' observed <- list(daily_pharyngitis_rate = 13, scarlet_fever_inc = 103,
+##'                  igas_inc = 93)
+##' pars <- example_gas_parameters(1)
+##' hydrogen_compare(state, observed, pars)
+##' hydrogen_compare(state * 5, observed, pars)
+hydrogen_compare <- function (state, observed, pars) {
+  if (!all(rownames(state) %in% names(observed))) {
+    stop("missing or misnamed data")
+  }
+
+  ll_pharyngitis <- gasworks:::ll_norm(observed$daily_pharyngitis_rate,
+                                       state["daily_pharyngitis_rate", ],
+                                       pars$k_gp, pars$exp_noise)
+  ll_scarlet_fever <- gasworks:::ll_nbinom(observed$scarlet_fever_inc,
+                                           state["scarlet_fever_inc", ],
+                                           pars$k_hpr, pars$exp_noise)
+  ll_igas <- gasworks:::ll_nbinom(observed$igas_inc, state["igas_inc", ],
+                                  pars$k_hpr, pars$exp_noise)
+  ll_pharyngitis + ll_scarlet_fever + ll_igas
+}
+
